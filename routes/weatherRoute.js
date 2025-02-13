cb1fc9d48fab4905775b7c52de3857fcf6060ace
thantzawaung023 <thantzinaung023@gmail.com>
2025-02-14 00:00:14 +0630

    [fix] route

--- routes/weatherRoute.js
+++ routes/weatherRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const accessTokenMiddleware = require("../middleware/accessTokenMiddleware");
-const { getWeatherByCity, getHourlyWeatherByCity } = require("../controllers/weatherController");
+const {
+  getWeatherByCity,
+  getHourlyWeatherByCity,
+} = require("../controllers/weatherController");
 
 router.get("/", accessTokenMiddleware, getWeatherByCity);
 
-router.get("/hourly", accessTokenMiddleware, getHourlyWeatherByCity);
+router.get("/weekly", accessTokenMiddleware, getHourlyWeatherByCity);
 
 module.exports = router;
